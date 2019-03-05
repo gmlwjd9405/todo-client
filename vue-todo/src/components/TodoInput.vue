64c18748094fa06e2e599b6ebf7def536eac86ef
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-05 17:19:19 +0900

    Refactor TodoInput component and Add styling

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-        <input type="text" v-model="newTodoItem">
-        <button v-on:click="addTodo">add</button>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <!-- <button v-on:click="addTodo">add</button> -->
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
     </div>
 </template>
 
@@ -17,14 +20,41 @@ export default {
         addTodo: function () {
             // 저장하는 로직
             localStorage.setItem(this.newTodoItem, this.newTodoItem);
-            this.newTodoItem = '';
+            this.clearInput();
         },
+        clearInput: function () {
+            this.newTodoItem = '';
+            
+        }
 
     }
 
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus {
+    outline: none;
+}
+.inputBox {
+    background-color: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
 </style>
