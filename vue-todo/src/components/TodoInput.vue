a00676b39a30e0db0ff29a6d0e824325c47708e3
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-05 17:02:01 +0900

    Update TodoInput component

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,11 +1,26 @@
 <template>
     <div>
-        input
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">add</button>
     </div>
 </template>
 
 <script>
 export default {
+    data: function () {
+        return {
+            newTodoItem: ""
+        }
+        
+    },
+    methods: {
+        addTodo: function () {
+            // 저장하는 로직
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.newTodoItem = '';
+        },
+
+    }
 
 }
 </script>
