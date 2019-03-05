a884727e3c7ed745d57b7b0269e6c462a8fa8a42
권희정 <heejeong@zuminternet.com>
2019-03-05 15:10:06 +0900

    Initial project and Register basic component

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,18 @@
+// component 당 template 안에 들어가는 HTML tag는 무조건 하나, child 형태로 아래에 추가 
+<template>
+    <!-- HTML -->
+    <div>
+        header
+    </div>
+</template>
+
+<script>
+export default {
+    // js script
+
+}
+</script>
+
+<style>
+    /* css */
+</style>
