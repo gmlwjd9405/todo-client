2e8042b3b81683880b61ab5deb8ce340a0f3be48
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-05 16:11:08 +0900

    Implement TodoHeader component

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,31 +1,50 @@
 <template>
-  <div id="app">
+<!-- HTML -->
+<div id="app">
     <TodoHeader></TodoHeader>
     <TodoInput></TodoInput>
     <TodoList></TodoList>
     <TodoFooter></TodoFooter>
-  </div>
+</div>
 </template>
 
 <script>
+// js script
 import TodoHeader from './components/TodoHeader.vue'
 import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-  components: {
-    // 컴포넌트 태그명 : 컴포넌트 내용
-    'TodoHeader': TodoHeader,
-    'TodoInput': TodoInput,
-    'TodoList': TodoList,
-    'TodoFooter': TodoFooter,
+    components: {
+        // 컴포넌트 태그명 : 컴포넌트 내용
+        'TodoHeader': TodoHeader,
+        'TodoInput': TodoInput,
+        'TodoList': TodoList,
+        'TodoFooter': TodoFooter,
 
-  }
+    }
 
 }
 </script>
 
 <style>
+/* css */
+body {
+    text-align: center;
+    background-color: #F6F6F6;
+}
 
+input {
+    border-style: groove;
+    width: 200px;
+}
+
+button {
+    border-style: groove;
+}
+
+.shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,18 +1,20 @@
 // component 당 template 안에 들어가는 HTML tag는 무조건 하나, child 형태로 아래에 추가 
 <template>
-    <!-- HTML -->
-    <div>
-        header
-    </div>
+<header>
+    <h1>TODO it!</h1>
+</header>
 </template>
 
 <script>
 export default {
-    // js script
 
 }
 </script>
 
-<style>
-    /* css */
+<style scoped>
+h1 {
+    color: #2F2852;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem; /* 바깥여백 */
+}
 </style>
