d0a7dcacd7334af6788a1dd707115ce8c17df742
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-05 17:48:18 +0900

    Add TodoList styling

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,11 @@
 <template>
 <div>
     <ul>
-        <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
             {{ todoItem }}
+        <span class="removeBtn" v-on:click="removeTodo">
+            <i class="fas fa-trash-alt"></i>
+        </span>
         </li>
     </ul>
 </div>
@@ -14,6 +17,11 @@ export default {
     data: function () {
         return {
             todoItems: []
+        }
+    },
+    method: {
+        removeTodo: function () {
+            
         }
     },
     created: function () {
@@ -31,5 +39,37 @@ export default {
 </script>
 
 <style>
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
+li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+.checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+}
+.checkBtnCompleted {
+    color: #b3adad;
+}
+.textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+}
+.removeBtn {
+    margin-left: auto;
+    color: #de4343
+}
 
 </style>
