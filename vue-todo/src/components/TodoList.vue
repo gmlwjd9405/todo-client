152fe4cd4a5f8e1042f0bb247b7080f70a87d128
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-05 17:39:24 +0900

    Implement data list using data in localStorage

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,11 +1,31 @@
 <template>
-    <div>
-        List
-    </div>
+<div>
+    <ul>
+        <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+            {{ todoItem }}
+        </li>
+    </ul>
+</div>
 </template>
 
 <script>
 export default {
+    // localStorage의 값을 담을 data
+    data: function () {
+        return {
+            todoItems: []
+        }
+    },
+    created: function () {
+        if (localStorage.length > 0) {
+            for (var i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+
+    }
 
 }
 </script>
