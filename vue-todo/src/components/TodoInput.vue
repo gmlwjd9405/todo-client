936fe81645a92920a3538eb780a4ba1350d18efb
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-06 09:32:17 +0900

    Register Modal component under Todo Input component

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -5,14 +5,25 @@
     <span class="addContainer" v-on:click="addTodo">
         <i class="fas fa-plus addBtn"></i>
     </span>
+
+    <Modal v-if="showModal" @close="showModal = false">
+        <!--    
+        you can use custom content here to overwrite
+        default content
+        -->
+        <h3 slot="header">custom header</h3>
+    </Modal>
 </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
     data: function () {
         return {
-            newTodoItem: ""
+            newTodoItem: "",
+            showModal: false
         }
     },
     methods: {
@@ -28,6 +39,10 @@ export default {
             this.newTodoItem = '';
 
         }
+    },
+    components: {
+        Modal: Modal,
+
     }
 }
 </script>
